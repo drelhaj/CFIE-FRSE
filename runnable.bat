e3be7cb4c33ba481de1d3e9a7b03d2ee8d0a1713
Dr Mahmoud EL-Haj <m.el-haj@lancaster.ac.uk>
2016-12-13 14:16:56 +0000

    Update runnable.bat
    updated to work with files containing spaces in the filename

--- runnable.bat
+++ runnable.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f %%a IN ('dir /b /s "pdfs\*.pdf"') do call java -Dfile.encoding=UTF-8 -jar CFIE.jar "%%a" keywords userKeywords.txt 
+for /f "delims=" %%a IN ('dir /b /s "pdfs\*.pdf"') do call java -Dfile.encoding=UTF-8 -jar CFIE.jar "%%a" keywords userKeywords.txt 
